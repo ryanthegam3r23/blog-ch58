c616c548222ac6bfa67e1bd715d382461e4b5164
ryanthegam3r23 <rypop04@gmail.com>
2025-08-05 22:19:41 -0500

    assignment 2

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 # Create your views here.
 # Class based views
@@ -9,3 +10,5 @@ class AboutPageView(TemplateView):
     template_name = "pages/about.html"
 
 #Function-based views
+#def about_view(request):
+ #   return render(request, "pages/about.html")
