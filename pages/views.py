fca72a3176a49a1a088853931087dd63d06ab05f
ryanthegam3r23 <rypop04@gmail.com>
2025-08-05 19:46:23 -0500

    assignment 1

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
 
 # Create your views here.
+# Class based views
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+
+#Function-based views
